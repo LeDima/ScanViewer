7111a0d6456f4be05b48c5d5cd7084b8abe3f02c
LeDima <484404@gmail.com>
2017-02-21 17:26:07 +0200

    commit 21.02.2017

--- WXpyViewer.py
+++ WXpyViewer.py
@@ -25,33 +25,92 @@ class RedirectText(object):
 class MainClass(MainFrame):
     def __init__( self, parent ):
         MainFrame.__init__( self, parent )
-#       self.OnTimer(None)
-        self.timer = wx.Timer(self, -1)
-        # update clock digits every second (1000ms)
-        self.timer.Start(500)
-        self.ser = self.open_serial_port('COM1')
-        self.Bind(wx.EVT_TIMER, self.OnTimer)
-        #redir=RedirectText(self.m_textCtrl3)
+
+        #redir=RedirectText(self.m_textLOG)   # Redirect print() to m_textLOG 
         #sys.stdout=redir
+
+        for i in range(4):
+            setval=["SET 1","SET 2","SET 3","SET 4"]
+            self.m_gridComment.SetCellAlignment(wx.ALIGN_CENTRE,i,0)
+            self.m_gridComment.SetCellValue(i,0,setval[i])
+            self.m_gridComment.SetReadOnly(i,0)
+            self.m_gridComment.SetCellFont(i,0, wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 92, False, wx.EmptyString ) )
+            #self.m_gridComment.SetCellValue(i,1,self.MainDict['Comment'][i])
+        for i in range(8):
+            if i%2==0:
+                self.m_gridValue.SetCellSize(i,0,2,1)
+            setval=["1","1","2","2","3","3","4","4"]
+            setval2=["X","Y","X","Y","X","Y","X","Y"]
+            self.m_gridValue.SetCellValue(i,0,setval[i])
+            self.m_gridValue.SetCellValue(i,1,setval2[i])
+            self.m_gridValue.SetCellAlignment(wx.ALIGN_CENTRE,i,0)
+            self.m_gridValue.SetCellAlignment(wx.ALIGN_CENTRE,i,1)
+            for j in range(7): 
+                self.m_gridValue.SetReadOnly(i,j)            
+            self.m_gridValue.SetCellFont(i,0, wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 92, False, wx.EmptyString ) )
+            self.m_gridValue.SetCellFont(i,1, wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 92, False, wx.EmptyString ) )
+        
+        try:
+            with open('ConfigandDate.txt', 'r', encoding='utf-8') as f:
+                self.MainDict = json.load(f) 
+        except (OSError, IOError):
+            self.NewMainDict={'SerialName':'COM1'
+                              ,'SerialSpeed':9600
+                              ,'SerialTimeout':.01
+                              ,'PeriodDate':500
+                              ,'ICPCONAdres':'01'
+                              ,'BeamDate':[[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001]
+                                          ,[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001]
+                                          ,[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001]
+                                          ,[0.001,0.001,0.001,0.001,0.001,0.001,0.001,0.001]]
+                              ,'Comment':['Comment 1','Comment 2','Comment 3','Comment 4']
+                             }
+            with open('ConfigandDate.txt', mode='w', encoding='utf-8') as f:
+                    json.dump(self.NewMainDict, f, indent=2)
+            with open('ConfigandDate.txt', 'r', encoding='utf-8') as f:
+                self.MainDict = json.load(f)
+        
+        self.timer = wx.Timer(self, -1)
+        self.timer.Start(self.MainDict['PeriodDate'])   # update clock digits every second ('PeriodDate' ms)
+        self.ser = self.open_serial_port(self.MainDict['SerialName'])
+        self.Bind(wx.EVT_TIMER, self.OnTimer)
+        
+        
+        
+        
+           
+            
+        
+        
+        
+        #self.m_gridComment.SetCellSize(1,1,2,1)
+        #self.m_gridComment.SetCellValue(3,1,'SDFSDF')
+        
+        
         #self.Centre()
+        
+    def SaveDate(self):
+        pass
     
     def OnTimer(self, event):
         #print(3)
         #print(self.ser)
-        try:
-            data_to_send = self.write_serial_port(self.ser, "#01")
-            print(type(data_to_send[0]))
-            data_to_send = [float(data_to_send[i:i+7]) for i in range(56) if i%7==0]
-            print(type(data_to_send[0]))
-            with open('DataSave.txt', mode='w', encoding='utf-8') as f:
-                json.dump(data_to_send, f, indent=2)
-        except:
-            data_to_send=[i for i in range(8)]
-       
-        print(type(data_to_send[0]))
-        with open('DataSave.txt', 'r', encoding='utf-8') as f:
-            entry = json.load(f) 
-        print(entry)
+#        try:
+        self.data_to_send = self.write_serial_port(self.ser, "#"+self.MainDict['ICPCONAdres'])
+        self.dataStr = [(self.data_to_send[i:i+7]) for i in range(56) if i%7==0]
+        self.dataFloat =[float(self.data_to_send[i:i+7]) for i in range(56) if i%7==0]
+#        except:
+#            self.dataFloat = [i*1.0 for i in range(8)]
+#            self.dataStr = [str(i*1.0) for i in range(8)]
+#        
+        for i in range(8):
+            self.m_gridValue.SetCellValue(i,2,str(self.dataStr[i]))
+            
+        
+        print(self.dataFloat)
+#        with open('DataSave.txt', 'r', encoding='utf-8') as f:
+#            entry = json.load(f) 
+#        print(entry)
         #print(type(data_to_send))
         #print(data_to_send[0])
         #print(4)
@@ -60,50 +119,43 @@ class MainClass(MainFrame):
        
         # time string can have characters 0..9, -, period, or space
         #self.m_textCtrl3.SetValue(str(sys.stdout))
-        if data_to_send[0]> 0:
-            self.m_textCtrl1.SetBackgroundColour(wx.RED)
-        
-        self.m_textCtrl1.SetValue(str(data_to_send[0]))
+#        if data_to_send[0]> 0:
+#            self.m_textCtrl1.SetBackgroundColour(wx.RED)
+#        
+#        self.m_textCtrl1.SetValue(str(data_to_send[0]))
 
     def open_serial_port(self,serial_name):
         try:
-            s = serial.Serial(serial_name, 9600)
-            s.timeout = .01;  # Timeout: 20 ms
+            s = serial.Serial(serial_name, self.MainDict['SerialSpeed'])
+            s.timeout = self.MainDict['SerialTimeout']; 
         except serial.SerialException:
             sys.stderr.write("Error opening the port {}".format(serial_name))
             sys.exit(1)
         print('Serial port connected')
         return s
 
-    def write_serial_port(self,s, cmd):
+    def write_serial_port(self,s, cmd="#01"):
         data = b""
-        print(3)
-        #print(cmd.encode("iso-8859-15"))
         CRC = format(sum([ord(ss) for ss in cmd]),'02X')
-        #print(CRC.encode("iso-8859-15"))
-        #print(CRC.encode("iso-8859-15"))
-        #print(cmd)
+        cmd1 = cmd.encode("iso-8859-15") + CRC.encode("iso-8859-15") + b"\r"
         
-        #i=0
+        i=0
         while b"\r" not in data:
-            cmd = cmd.encode("iso-8859-15") + CRC.encode("iso-8859-15") + b"\r"
-            s.write(cmd)
+            s.write(cmd1)
             data += s.read(60)
-        #    print(data)
-        #    i+=1
-        #   if i>=2:
-        #        data="-100"
-        #        break
-        #print(data)
-        #CRC2= format(sum([ord(ss) for ss in data[:-3].decode("iso-8859-15")]),'02X')[-2:]
-        #print(CRC2)
-        #if data[-3:-1] == format(sum([ord(ss) for ss in data[:-3]]),'02X')[-2:]:
+            i+=1
+            if i>=2:
+                print("Error connected device")  
+                break
+        
         if data[-3:-1].decode("iso-8859-15") == format(sum([ord(ss) for ss in data[:-3].decode("iso-8859-15")]),'02X')[-2:]:
             data=data[1:-3].decode("iso-8859-15")
-        #    print(4)
         else:
-            data=None
-        #print(data)
+            if i<2:
+                print("Error CRC ")
+            data='+0.0000+0.0000+0.0000+0.0000+0.0000+0.0000+0.0000+0.0000'
+                      
+#        print(data)
         return data
 
 
@@ -113,9 +165,9 @@ class MainClass(MainFrame):
 if __name__ == "__main__":	
     app = wx.App()
     wnd = MainClass(None)
-    print(1)
+    #print(1)
     wnd.Show(True)
-    print(2)
+    #print(2)
 #    i=0
 #    while i < 10:
 #        i+=1
--- wxFormScavViewer.py
+++ wxFormScavViewer.py
@@ -9,6 +9,7 @@
 
 import wx
 import wx.xrc
+import wx.grid
 
 ###########################################################################
 ## Class MainFrame
@@ -21,18 +22,8 @@ class MainFrame ( wx.Frame ):
 		
 		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
 		
-		self.m_menubar1 = wx.MenuBar( 0 )
-		self.File = wx.Menu()
-		self.m_menuItem1 = wx.MenuItem( self.File, wx.ID_ANY, u"Open", wx.EmptyString, wx.ITEM_NORMAL )
-		self.File.AppendItem( self.m_menuItem1 )
-		
-		self.m_menubar1.Append( self.File, u"File" ) 
-		
-		self.SetMenuBar( self.m_menubar1 )
-		
 		sbSizer1 = wx.StaticBoxSizer( wx.StaticBox( self, wx.ID_ANY, u"label" ), wx.VERTICAL )
 		
-		sbSizer1.SetMinSize( wx.Size( 400,600 ) ) 
 		self.m_button1 = wx.Button( sbSizer1.GetStaticBox(), wx.ID_ANY, u"MyButton", wx.DefaultPosition, wx.DefaultSize, 0 )
 		sbSizer1.Add( self.m_button1, 0, wx.ALL, 5 )
 		
@@ -41,26 +32,92 @@ class MainFrame ( wx.Frame ):
 		
 		sbSizer1.Add( self.m_textCtrl1, 0, wx.ALL, 5 )
 		
-		self.m_textCtrl3 = wx.TextCtrl( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.Point( 300,200 ), wx.Size( 400,200 ), wx.TE_MULTILINE )
-		self.m_textCtrl3.SetMinSize( wx.Size( 400,200 ) )
+		self.m_gridValue = wx.grid.Grid( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
 		
-		sbSizer1.Add( self.m_textCtrl3, 0, wx.ALL, 5 )
+		# Grid
+		self.m_gridValue.CreateGrid( 8, 7 )
+		self.m_gridValue.EnableEditing( True )
+		self.m_gridValue.EnableGridLines( True )
+		self.m_gridValue.EnableDragGridSize( False )
+		self.m_gridValue.SetMargins( 0, 0 )
+		
+		# Columns
+		self.m_gridValue.SetColSize( 0, 72 )
+		self.m_gridValue.SetColSize( 1, 52 )
+		self.m_gridValue.SetColSize( 2, 106 )
+		self.m_gridValue.SetColSize( 3, 80 )
+		self.m_gridValue.SetColSize( 4, 80 )
+		self.m_gridValue.SetColSize( 5, 80 )
+		self.m_gridValue.SetColSize( 6, 80 )
+		self.m_gridValue.EnableDragColMove( False )
+		self.m_gridValue.EnableDragColSize( True )
+		self.m_gridValue.SetColLabelSize( 30 )
+		self.m_gridValue.SetColLabelValue( 0, u"GUN" )
+		self.m_gridValue.SetColLabelValue( 1, u"X, Y" )
+		self.m_gridValue.SetColLabelValue( 2, u"Present value" )
+		self.m_gridValue.SetColLabelValue( 3, u"SET 1" )
+		self.m_gridValue.SetColLabelValue( 4, u"SET 2" )
+		self.m_gridValue.SetColLabelValue( 5, u"SET 3" )
+		self.m_gridValue.SetColLabelValue( 6, u"SET 4" )
+		self.m_gridValue.SetColLabelValue( 7, u"Present" )
+		self.m_gridValue.SetColLabelValue( 8, u"Present" )
+		self.m_gridValue.SetColLabelValue( 9, u"Present" )
+		self.m_gridValue.SetColLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
+		
+		# Rows
+		self.m_gridValue.EnableDragRowSize( True )
+		self.m_gridValue.SetRowLabelSize( 0 )
+		self.m_gridValue.SetRowLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
+		
+		# Label Appearance
+		self.m_gridValue.SetLabelFont( wx.Font( wx.NORMAL_FONT.GetPointSize(), 70, 90, 92, False, wx.EmptyString ) )
+		
+		# Cell Defaults
+		self.m_gridValue.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
+		sbSizer1.Add( self.m_gridValue, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_gridComment = wx.grid.Grid( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+		
+		# Grid
+		self.m_gridComment.CreateGrid( 4, 2 )
+		self.m_gridComment.EnableEditing( True )
+		self.m_gridComment.EnableGridLines( True )
+		self.m_gridComment.EnableDragGridSize( False )
+		self.m_gridComment.SetMargins( 0, 0 )
+		
+		# Columns
+		self.m_gridComment.SetColSize( 0, 50 )
+		self.m_gridComment.SetColSize( 1, 550 )
+		self.m_gridComment.EnableDragColMove( False )
+		self.m_gridComment.EnableDragColSize( True )
+		self.m_gridComment.SetColLabelSize( 30 )
+		self.m_gridComment.SetColLabelValue( 0, u"SET" )
+		self.m_gridComment.SetColLabelValue( 1, u"Comment" )
+		self.m_gridComment.SetColLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
+		
+		# Rows
+		self.m_gridComment.EnableDragRowSize( True )
+		self.m_gridComment.SetRowLabelSize( 0 )
+		self.m_gridComment.SetRowLabelAlignment( wx.ALIGN_CENTRE, wx.ALIGN_CENTRE )
+		
+		# Label Appearance
+		
+		# Cell Defaults
+		self.m_gridComment.SetDefaultCellAlignment( wx.ALIGN_LEFT, wx.ALIGN_TOP )
+		sbSizer1.Add( self.m_gridComment, 0, wx.ALL|wx.EXPAND, 5 )
+		
+		self.m_textLOG = wx.TextCtrl( sbSizer1.GetStaticBox(), wx.ID_ANY, wx.EmptyString, wx.Point( 0,0 ), wx.Size( -1,-1 ), wx.TE_MULTILINE )
+		self.m_textLOG.SetMinSize( wx.Size( -1,150 ) )
+		
+		sbSizer1.Add( self.m_textLOG, 0, wx.EXPAND|wx.ALL, 5 )
 		
 		
 		self.SetSizer( sbSizer1 )
 		self.Layout()
 		
 		self.Centre( wx.BOTH )
-		
-		# Connect Events
-		self.Bind( wx.EVT_MENU, self.fileopen, id = self.m_menuItem1.GetId() )
 	
 	def __del__( self ):
 		pass
-	
-	
-	# Virtual event handlers, overide them in your derived class
-	def fileopen( self, event ):
-		event.Skip()
 	
 
